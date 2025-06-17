1d887f6b17c96166d5ffa5d937b12b03847d6a3b
Matthew Waters <matthew@centricular.com>
2025-06-17 23:02:18 +1000

    client: expand example to support TCP connection to the server

--- turn-client-proto/examples/turn.rs
+++ turn-client-proto/examples/turn.rs
@@ -9,15 +9,16 @@
 //! An example showing how a TURN client can be implemented.
 
 use turn_client_proto::types::TurnCredentials;
-use turn_client_proto::{
-    CreatePermissionError, TurnClient as Client, TurnEvent, TurnPollRet, TurnRecvRet,
-};
+use turn_client_proto::{CreatePermissionError, TurnClient, TurnEvent, TurnPollRet, TurnRecvRet};
 
 use stun_proto::agent::{DelayedTransmitBuild, StunError, Transmit};
+use turn_types::stun::data::Data;
 use turn_types::stun::TransportType;
 
-use std::io;
-use std::net::{IpAddr, SocketAddr};
+use clap::{Parser, ValueEnum};
+
+use std::io::{self, Read, Write};
+use std::net::{IpAddr, SocketAddr, TcpStream};
 use std::sync::mpsc::SyncSender;
 use std::sync::{Arc, Condvar, Mutex};
 use std::time::Instant;
@@ -46,28 +47,42 @@ fn init_logs() {
     tracing::subscriber::set_global_default(registry).unwrap();
 }
 
-#[derive(Debug)]
-struct TurnClient {
-    socket: Arc<UdpSocket>,
-    inner: Arc<(Mutex<ClientInner>, Condvar)>,
+trait Client<T: AsRef<[u8]> + std::fmt::Debug> {
+    fn close(&self);
+    fn create_permission(
+        &self,
+        transport: TransportType,
+        peer_addr: IpAddr,
+    ) -> Result<(), CreatePermissionError>;
+    fn send(
+        &self,
+        transport: TransportType,
+        peer_addr: SocketAddr,
+        data: T,
+    ) -> Result<(), StunError>;
 }
 
-impl TurnClient {
-    fn new_udp(
+#[derive(Clone, Debug)]
+struct TurnClientUdp {
+    socket: Arc<UdpSocket>,
+    inner: Arc<(Mutex<TurnClientUdpInner>, Condvar)>,
+}
+
+impl TurnClientUdp {
+    fn new(
         socket: UdpSocket,
         to: SocketAddr,
         credentials: TurnCredentials,
         events_sender: SyncSender<TurnEvent>,
     ) -> Self {
         let local_addr = socket.local_addr().unwrap();
-        let client = Client::allocate(TransportType::Udp, local_addr, to, credentials);
-        let inner = Arc::new((Mutex::new(ClientInner { client }), Condvar::new()));
-        let weak_inner = Arc::downgrade(&inner);
+        let client = TurnClient::allocate(TransportType::Udp, local_addr, to, credentials);
+        let inner = Arc::new((Mutex::new(TurnClientUdpInner { client }), Condvar::new()));
         let socket = Arc::new(socket);
 
         let socket_clone = socket.clone();
         let inner_s = inner.clone();
-        let recv_task = std::thread::spawn(move || loop {
+        std::thread::spawn(move || loop {
             let mut data = [0; 2048];
             let Ok((size, from)) = socket_clone.recv_from(&mut data) else {
                 break;
@@ -103,7 +118,7 @@ impl TurnClient {
 
         let inner_s = inner.clone();
         let socket_clone = socket.clone();
-        let poll_task = std::thread::spawn(move || loop {
+        std::thread::spawn(move || loop {
             let now = Instant::now();
             let mut inner = inner_s.0.lock().unwrap();
             let lowest_wait = match inner.client.poll(now) {
@@ -114,7 +129,7 @@ impl TurnClient {
             };
 
             if let Some(event) = inner.client.poll_event() {
-                events_sender.send(event);
+                events_sender.send(event).unwrap();
             }
             let mut sent = false;
             while let Some(transmit) = inner.client.poll_transmit(now) {
@@ -124,12 +139,14 @@ impl TurnClient {
             if sent {
                 continue;
             }
-            inner_s.1.wait_timeout(inner, now - lowest_wait);
+            let _ = inner_s.1.wait_timeout(inner, now - lowest_wait);
         });
         Self { socket, inner }
     }
+}
 
-    pub fn close(&self) {
+impl<T: AsRef<[u8]> + std::fmt::Debug> Client<T> for TurnClientUdp {
+    fn close(&self) {
         let transmit = {
             let mut inner = self.inner.0.lock().unwrap();
             inner.client.delete(Instant::now())
@@ -140,7 +157,7 @@ impl TurnClient {
         }
     }
 
-    pub fn create_permission(
+    fn create_permission(
         &self,
         transport: TransportType,
         peer_addr: IpAddr,
@@ -155,7 +172,7 @@ impl TurnClient {
         Ok(())
     }
 
-    pub fn send<T: AsRef<[u8]> + std::fmt::Debug>(
+    fn send(
         &self,
         transport: TransportType,
         peer_addr: SocketAddr,
@@ -179,25 +196,216 @@ impl TurnClient {
 }
 
 #[derive(Debug)]
-struct ClientInner {
-    client: Client,
+struct TurnClientUdpInner {
+    client: TurnClient,
+}
+
+#[derive(Clone, Debug)]
+struct TurnClientTcp {
+    send_sender: SyncSender<Data<'static>>,
+    inner: Arc<(Mutex<TurnClientTcpInner>, Condvar)>,
+}
+
+impl TurnClientTcp {
+    fn new(
+        mut socket: TcpStream,
+        to: SocketAddr,
+        credentials: TurnCredentials,
+        events_sender: SyncSender<TurnEvent>,
+    ) -> Self {
+        let local_addr = socket.local_addr().unwrap();
+        let remote_addr = to;
+        let client = TurnClient::allocate(TransportType::Tcp, local_addr, remote_addr, credentials);
+        let inner = Arc::new((Mutex::new(TurnClientTcpInner { client }), Condvar::new()));
+
+        let mut socket_clone = socket.try_clone().unwrap();
+        let inner_s = inner.clone();
+        std::thread::spawn(move || loop {
+            let mut data = [0; 2048];
+            let Ok(size) = socket_clone.read(&mut data) else {
+                break;
+            };
+            {
+                let mut inner = inner_s.0.lock().unwrap();
+                let now = Instant::now();
+                let ret = inner.client.recv(
+                    Transmit::new(&data[..size], TransportType::Tcp, remote_addr, local_addr),
+                    now,
+                );
+                println!("recv ret {ret:?}");
+                match ret {
+                    TurnRecvRet::Ignored(_) => continue,
+                    TurnRecvRet::Handled => {
+                        inner_s.1.notify_one();
+                        continue;
+                    }
+                    TurnRecvRet::PeerData {
+                        data,
+                        transport,
+                        peer,
+                    } => {
+                        println!(
+                            "received {} bytes from {peer:?} using {transport:?}",
+                            data.len()
+                        );
+                        continue;
+                    }
+                }
+            }
+        });
+
+        let (send_sender, send_recv) = std::sync::mpsc::sync_channel::<Data<'static>>(8);
+        std::thread::spawn(move || loop {
+            while let Ok(recv) = send_recv.recv() {
+                let socket = &mut socket;
+                socket.write_all(&recv).unwrap();
+            }
+        });
+
+        let inner_s = inner.clone();
+        let sender = send_sender.clone();
+        std::thread::spawn(move || loop {
+            let now = Instant::now();
+            let mut inner = inner_s.0.lock().unwrap();
+            let lowest_wait = match inner.client.poll(now) {
+                TurnPollRet::WaitUntil(wait) => wait,
+                TurnPollRet::Closed => {
+                    break;
+                }
+            };
+
+            if let Some(event) = inner.client.poll_event() {
+                events_sender.send(event).unwrap();
+            }
+            let mut sent = false;
+            while let Some(transmit) = inner.client.poll_transmit(now) {
+                sent = true;
+                sender.send(transmit.data.into_owned()).unwrap();
+            }
+            if sent {
+                continue;
+            }
+            let _ = inner_s.1.wait_timeout(inner, now - lowest_wait);
+        });
+        Self { send_sender, inner }
+    }
+}
+
+impl<T: AsRef<[u8]> + std::fmt::Debug> Client<T> for TurnClientTcp {
+    fn close(&self) {
+        let transmit = {
+            let mut inner = self.inner.0.lock().unwrap();
+            inner.client.delete(Instant::now())
+        };
+
+        if let Some(transmit) = transmit {
+            self.send_sender.send(transmit.data.into_owned()).unwrap();
+        }
+        // TODO: actually close the TCP connection.
+    }
+
+    fn create_permission(
+        &self,
+        transport: TransportType,
+        peer_addr: IpAddr,
+    ) -> Result<(), CreatePermissionError> {
+        let transmit = {
+            let mut inner = self.inner.0.lock().unwrap();
+            inner
+                .client
+                .create_permission(transport, peer_addr, Instant::now())?
+        };
+        self.send_sender.send(transmit.data.into_owned()).unwrap();
+        Ok(())
+    }
+
+    fn send(
+        &self,
+        transport: TransportType,
+        peer_addr: SocketAddr,
+        data: T,
+    ) -> Result<(), StunError> {
+        let transmit = {
+            let mut inner = self.inner.0.lock().unwrap();
+            let transmit = inner
+                .client
+                .send_to(transport, peer_addr, data, Instant::now())?;
+            Transmit::new(
+                Data::from(transmit.data.build().as_ref()).into_owned(),
+                transmit.transport,
+                transmit.from,
+                transmit.to,
+            )
+        };
+        self.send_sender.send(transmit.data).unwrap();
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+struct TurnClientTcpInner {
+    client: TurnClient,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug, ValueEnum)]
+enum Transport {
+    Udp,
+    Tcp,
+}
+
+impl From<Transport> for TransportType {
+    fn from(value: Transport) -> Self {
+        match value {
+            Transport::Udp => Self::Udp,
+            Transport::Tcp => Self::Tcp,
+        }
+    }
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Cli {
+    #[arg(short, long, value_enum)]
+    transport: Transport,
+    #[arg(short, long)]
+    server: SocketAddr,
+    #[arg(short, long)]
+    user: String,
+    #[arg(short, long)]
+    password: String,
+    #[arg(short = 'a', long)]
+    peer: SocketAddr,
 }
 
 fn main() -> io::Result<()> {
     init_logs();
-    let turn_server = "127.0.0.1:3478".parse().unwrap();
-    let credentials = TurnCredentials::new("coturn", "password");
-    let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
+
+    let cli = Cli::parse();
+    let transport: TransportType = cli.transport.into();
+    let credentials = TurnCredentials::new(&cli.user, &cli.password);
     let (events_sender, events_recv) = std::sync::mpsc::sync_channel(8);
 
-    let client = TurnClient::new_udp(socket, turn_server, credentials, events_sender);
+    let client = match transport {
+        TransportType::Udp => {
+            let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
+            let client = TurnClientUdp::new(socket, cli.server, credentials, events_sender);
+            Box::new(client) as Box<dyn Client<_>>
+        }
+        TransportType::Tcp => {
+            let socket = TcpStream::connect(cli.server).unwrap();
+            let client = TurnClientTcp::new(socket, cli.server, credentials, events_sender);
+            Box::new(client) as Box<dyn Client<_>>
+        }
+    };
 
-    let peer_addr: SocketAddr = "127.0.0.1:9999".parse().unwrap();
+    let peer_addr = cli.peer;
 
     while let Ok(event) = events_recv.recv() {
         match event {
             TurnEvent::AllocationCreated(_, _) => {
-                client.create_permission(TransportType::Udp, peer_addr.ip());
+                client
+                    .create_permission(TransportType::Udp, peer_addr.ip())
+                    .unwrap();
             }
             TurnEvent::AllocationCreateFailed => {
                 error!("Failed to create allocation");
@@ -205,7 +413,7 @@ fn main() -> io::Result<()> {
             }
             TurnEvent::PermissionCreated(transport, _peer_addr) => {
                 let data = b"Hello from turn.\n";
-                client.send(transport, peer_addr, data);
+                client.send(transport, peer_addr, data).unwrap();
                 client.close();
             }
             TurnEvent::PermissionCreateFailed(transport, peer_addr) => {
