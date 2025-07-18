fd4db8cd4abb6d6c551a6148c972a34fbcd36031
Matthew Waters <matthew@centricular.com>
2025-07-18 15:36:38 +1000

    client: add support for TLS connection using rustls

--- turn-client-proto/examples/turn.rs
+++ turn-client-proto/examples/turn.rs
@@ -36,7 +36,10 @@
 
 #![cfg(not(tarpaulin))]
 
+use rustls::ClientConfig;
+use rustls_platform_verifier::ConfigVerifierExt;
 use turn_client_proto::prelude::*;
+use turn_client_proto::rustls::TurnClientTls;
 use turn_client_proto::tcp::TurnClientTcp;
 use turn_client_proto::types::TurnCredentials;
 use turn_client_proto::udp::{
@@ -372,10 +375,240 @@ struct ClientTcpInner {
     client: TurnClientTcp,
 }
 
+#[derive(Clone, Debug)]
+struct ClientTls {
+    send_sender: SyncSender<Data<'static>>,
+    inner: Arc<(Mutex<ClientTlsInner>, Condvar)>,
+}
+
+use rustls::crypto::aws_lc_rs as crypto_provider;
+mod danger {
+    use rustls::client::danger::HandshakeSignatureValid;
+    use rustls::crypto::{verify_tls12_signature, verify_tls13_signature, CryptoProvider};
+    use rustls::pki_types::{CertificateDer, ServerName, UnixTime};
+    use rustls::DigitallySignedStruct;
+
+    #[derive(Debug)]
+    pub struct NoCertificateVerification(CryptoProvider);
+
+    impl NoCertificateVerification {
+        pub fn new(provider: CryptoProvider) -> Self {
+            Self(provider)
+        }
+    }
+
+    impl rustls::client::danger::ServerCertVerifier for NoCertificateVerification {
+        fn verify_server_cert(
+            &self,
+            _end_entity: &CertificateDer<'_>,
+            _intermediates: &[CertificateDer<'_>],
+            _server_name: &ServerName<'_>,
+            _ocsp: &[u8],
+            _now: UnixTime,
+        ) -> Result<rustls::client::danger::ServerCertVerified, rustls::Error> {
+            Ok(rustls::client::danger::ServerCertVerified::assertion())
+        }
+
+        fn verify_tls12_signature(
+            &self,
+            message: &[u8],
+            cert: &CertificateDer<'_>,
+            dss: &DigitallySignedStruct,
+        ) -> Result<HandshakeSignatureValid, rustls::Error> {
+            verify_tls12_signature(
+                message,
+                cert,
+                dss,
+                &self.0.signature_verification_algorithms,
+            )
+        }
+
+        fn verify_tls13_signature(
+            &self,
+            message: &[u8],
+            cert: &CertificateDer<'_>,
+            dss: &DigitallySignedStruct,
+        ) -> Result<HandshakeSignatureValid, rustls::Error> {
+            verify_tls13_signature(
+                message,
+                cert,
+                dss,
+                &self.0.signature_verification_algorithms,
+            )
+        }
+
+        fn supported_verify_schemes(&self) -> Vec<rustls::SignatureScheme> {
+            self.0.signature_verification_algorithms.supported_schemes()
+        }
+    }
+}
+
+impl ClientTls {
+    fn new(
+        mut socket: TcpStream,
+        to: SocketAddr,
+        credentials: TurnCredentials,
+        events_sender: SyncSender<TurnEvent>,
+        insecure_tls: bool,
+    ) -> Self {
+        let local_addr = socket.local_addr().unwrap();
+        let remote_addr = to;
+        let config = if insecure_tls {
+            ClientConfig::builder()
+                .dangerous()
+                .with_custom_certificate_verifier(Arc::new(danger::NoCertificateVerification::new(
+                    crypto_provider::default_provider(),
+                )))
+                .with_no_client_auth()
+        } else {
+            ClientConfig::with_platform_verifier()
+        };
+        let client = TurnClientTls::allocate(
+            local_addr,
+            remote_addr,
+            credentials,
+            remote_addr.ip().into(),
+            Arc::new(config),
+        );
+        let inner = Arc::new((Mutex::new(ClientTlsInner { client }), Condvar::new()));
+
+        let mut socket_clone = socket.try_clone().unwrap();
+        let inner_s = inner.clone();
+        std::thread::spawn(move || loop {
+            let mut data = [0; 2048];
+            let Ok(size) = socket_clone.read(&mut data) else {
+                break;
+            };
+            {
+                let mut inner = inner_s.0.lock().unwrap();
+                let now = Instant::now();
+                let ret = inner.client.recv(
+                    Transmit::new(&data[..size], TransportType::Tcp, remote_addr, local_addr),
+                    now,
+                );
+                println!("recv ret {ret:?}");
+                match ret {
+                    TurnRecvRet::Ignored(_) => continue,
+                    TurnRecvRet::Handled => {
+                        inner_s.1.notify_one();
+                        continue;
+                    }
+                    TurnRecvRet::PeerData(peer_data) => {
+                        let data = peer_data.data();
+                        let len = data.len();
+                        let s = match std::str::from_utf8(data) {
+                            Ok(s) => s.to_string(),
+                            Err(_e) => format!("{:x?}", data),
+                        };
+                        println!(
+                            "received {len} bytes from {:?} using {:?}: {s:?}",
+                            peer_data.peer, peer_data.transport
+                        );
+                        continue;
+                    }
+                }
+            }
+        });
+
+        let (send_sender, send_recv) = std::sync::mpsc::sync_channel::<Data<'static>>(8);
+        std::thread::spawn(move || loop {
+            while let Ok(recv) = send_recv.recv() {
+                let socket = &mut socket;
+                socket.write_all(&recv).unwrap();
+            }
+        });
+
+        let inner_s = inner.clone();
+        let sender = send_sender.clone();
+        std::thread::spawn(move || loop {
+            let now = Instant::now();
+            let mut inner = inner_s.0.lock().unwrap();
+            let lowest_wait = match inner.client.poll(now) {
+                TurnPollRet::WaitUntil(wait) => wait,
+                TurnPollRet::Closed => {
+                    break;
+                }
+            };
+
+            if let Some(event) = inner.client.poll_event() {
+                events_sender.send(event).unwrap();
+            }
+            let mut sent = false;
+            while let Some(transmit) = inner.client.poll_transmit(now) {
+                sent = true;
+                sender.send(transmit.data.into_owned()).unwrap();
+            }
+            if sent {
+                continue;
+            }
+            let _ = inner_s.1.wait_timeout(inner, lowest_wait - now);
+        });
+        Self { send_sender, inner }
+    }
+}
+
+impl<T: AsRef<[u8]> + std::fmt::Debug> Client<T> for ClientTls {
+    fn close(&self) {
+        let mut inner = self.inner.0.lock().unwrap();
+        let _ = inner.client.delete(Instant::now());
+
+        self.inner.1.notify_one();
+        // TODO: actually close the TCP connection.
+    }
+
+    fn create_permission(
+        &self,
+        transport: TransportType,
+        peer_addr: IpAddr,
+    ) -> Result<(), CreatePermissionError> {
+        let mut inner = self.inner.0.lock().unwrap();
+        inner
+            .client
+            .create_permission(transport, peer_addr, Instant::now())?;
+        self.inner.1.notify_one();
+        Ok(())
+    }
+
+    fn send(
+        &self,
+        transport: TransportType,
+        peer_addr: SocketAddr,
+        data: T,
+    ) -> Result<(), SendError> {
+        let transmit = {
+            let mut inner = self.inner.0.lock().unwrap();
+            let transmit = inner
+                .client
+                .send_to(transport, peer_addr, data, Instant::now())?
+                .unwrap();
+            Transmit::new(
+                Data::from(transmit.data.build().into_boxed_slice()).into_owned(),
+                transmit.transport,
+                transmit.from,
+                transmit.to,
+            )
+        };
+        self.send_sender.send(transmit.data).unwrap();
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+struct ClientTlsInner {
+    client: TurnClientTls,
+}
+
 #[derive(Clone, Copy, PartialEq, Eq, Debug, ValueEnum)]
 enum Transport {
     Udp,
     Tcp,
+    Tls,
+}
+
+impl Transport {
+    fn is_tls(&self) -> bool {
+        matches!(self, Self::Tls)
+    }
 }
 
 impl From<Transport> for TransportType {
@@ -383,6 +616,7 @@ impl From<Transport> for TransportType {
         match value {
             Transport::Udp => Self::Udp,
             Transport::Tcp => Self::Tcp,
+            Transport::Tls => Self::Tcp,
         }
     }
 }
@@ -435,12 +669,15 @@ struct Cli {
         help = "number of seconds to wait between messages"
     )]
     delay: u64,
+    #[arg(long, default_value = "false", help = "Insecure TLS")]
+    insecure_tls: bool,
 }
 
 fn main() -> io::Result<()> {
     init_logs();
 
     let cli = Cli::parse();
+    let is_tls = cli.transport.is_tls();
     let transport: TransportType = cli.transport.into();
     let credentials = TurnCredentials::new(&cli.user, &cli.password);
     let (events_sender, events_recv) = std::sync::mpsc::sync_channel(8);
@@ -453,8 +690,19 @@ fn main() -> io::Result<()> {
         }
         TransportType::Tcp => {
             let socket = TcpStream::connect(cli.server).unwrap();
-            let client = ClientTcp::new(socket, cli.server, credentials, events_sender);
-            Box::new(client) as Box<dyn Client<_>>
+            if is_tls {
+                let client = ClientTls::new(
+                    socket,
+                    cli.server,
+                    credentials,
+                    events_sender,
+                    cli.insecure_tls,
+                );
+                Box::new(client) as Box<dyn Client<_>>
+            } else {
+                let client = ClientTcp::new(socket, cli.server, credentials, events_sender);
+                Box::new(client) as Box<dyn Client<_>>
+            }
         }
     };
 
